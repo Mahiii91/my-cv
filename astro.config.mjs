1f655c730d2fa5b935f1ca02efe5f912e684c6ad
mahi <mahiramezani02@gmail.com>
2026-05-27 16:46:12 +0200

    Set up GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://mahiii91.github.io',
+	base: '/my-cv',
+});
